4b48ea891d8177d1a213ce4a36f390895092a13f
GeunYong <rmsdyd1926@naver.com>
2024-06-06 19:15:40 +0900

    Initial commit

---
+++ Source/GameAI/ParentsAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ParentsAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEAI_API AParentsAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
